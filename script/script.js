d34e1e3ca11d567c211b8fed81cda7f903a098fc
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-08-14 10:46:39 +0800

    nav mobile modal overlay

---
+++ script/script.js
@@ -0,0 +1,16 @@
+const burgericon = document.getElementById('burgericon');
+const navmobilecontainer = document.getElementById('navmobilecontainer');
+const navmobile = document.getElementById('navmobile');
+
+burgericon.addEventListener('click', () => {
+    navmobilecontainer.classList.toggle('hidden');
+});
+
+
+navmobilecontainer.addEventListener('click', function (event) {
+    event.stopPropagation();
+    if (!navmobile.contains(event.target)) {
+        navmobilecontainer.classList.add('hidden');
+    }
+
+  });
