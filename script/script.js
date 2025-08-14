2b08274fcbcd75405c6cd637b50eb03571237e5f
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-08-15 07:18:48 +0800

    progress duel game

--- script/script.js
+++ script/script.js
@@ -41,6 +41,15 @@ setInterval(() => {
     opponentMagicToken++;
     magicMeterLeft.style.width = magicToken + '%';
     magicMeterRight.style.width = opponentMagicToken + '%';
+
+    if(magicToken<=0){
+        magicDuelOutcomeContainer.classList.remove('hidden');
+        magicDuelOutcome.classList.remove('hidden');
+        magicDuelResult.textContent = 'You lose!';
+        magicDuelMessage.textContent = 'Better luck next time!';
+        magicDuelOutcomeLoseButton.classList.remove('hidden');
+    }
+
 }, 5000/magicDuelLevel.textContent);
 
 abraKadabraButton.addEventListener('click', () => {
@@ -48,4 +57,51 @@ abraKadabraButton.addEventListener('click', () => {
     opponentMagicToken -= 5;
     magicMeterLeft.style.width = magicToken + '%';
     magicMeterRight.style.width = opponentMagicToken + '%';
+    if(magicToken>=100){
+        magicDuelOutcomeContainer.classList.remove('hidden');
+        magicDuelOutcome.classList.remove('hidden');
+        magicDuelResult.textContent = 'You win!';
+        magicDuelMessage.textContent = 'Congratulations!';
+        magicDuelOutcomeWinButton.classList.remove('hidden');
+
+    }
+    
 });
+
+const magicDuelOutcomeContainer = document.getElementById('magicDuelOutcomeContainer');
+const magicDuelOutcome = document.getElementById('magicDuelOutcome');
+const magicDuelResult = document.getElementById('magicDuelResult');
+const magicDuelMessage = document.getElementById('magicDuelMessage');
+const magicDuelOutcomeWinButton = document.getElementById('magicDuelOutcomeWinButton');
+const magicDuelOutcomeLoseButton = document.getElementById('magicDuelOutcomeLoseButton');
+
+// switch(true) {
+//     case (magicToken <= 0):
+//         magicDuelOutcomeContainer.classList.remove('hidden');
+//         magicDuelOutcome.classList.remove('hidden');
+//         magicDuelResult.textContent = 'You lose!';
+//         magicDuelMessage.textContent = 'Better luck next time!';
+//         magicDuelOutcomeLoseButton.textContent = 'Try Again';
+//         break;
+//     case (magicToken >= 100):
+//         magicDuelOutcomeContainer.classList.remove('hidden');
+//         magicDuelOutcome.classList.remove('hidden');
+//         magicDuelResult.textContent = 'You win!';
+//         magicDuelMessage.textContent = 'Congratulations!';
+//         magicDuelOutcomeWinButton.textContent = 'Go to the Next Level';
+//         break;
+// }
+// console.log(`magicToken: ${magicToken}`);
+// if(magicToken>=100){
+//         magicDuelOutcomeContainer.classList.remove('hidden');
+//         magicDuelOutcome.classList.remove('hidden');
+//         magicDuelResult.textContent = 'You win!';
+//         magicDuelMessage.textContent = 'Congratulations!';
+//         magicDuelOutcomeWinButton.textContent = 'Go to the Next Level';
+// }else if(magicToken<=0){
+//         magicDuelOutcomeContainer.classList.remove('hidden');
+//         magicDuelOutcome.classList.remove('hidden');
+//         magicDuelResult.textContent = 'You lose!';
+//         magicDuelMessage.textContent = 'Better luck next time!';
+//         magicDuelOutcomeLoseButton.textContent = 'Try Again';
+// }
